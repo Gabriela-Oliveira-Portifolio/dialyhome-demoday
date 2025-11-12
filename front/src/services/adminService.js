fb890fccc92dea8f5e9771fab3be4a9837ca987a
gabrielaconstante <gabrielaconstante04@gmail.com>
2025-11-12 18:19:28 -0300

    atualizacao rota api login

--- front/src/services/adminService.js
+++ front/src/services/adminService.js
@@ -1,7 +1,7 @@
 // front/src/services/adminService.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://dialyhome.com.br/api';
 
 // Configurar interceptor para adicionar token
 const getAuthHeader = () => {
